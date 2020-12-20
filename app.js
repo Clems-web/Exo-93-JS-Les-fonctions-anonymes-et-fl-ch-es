edf422bc58d0af28d4bf51f1094c2470bb26a78a
Clems-web <clement.servais1@gmail.com>
2020-12-20 20:00:10 +0100

    Correction première partie de l'exo et simplification de la deuxième

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 let buttonAlert = document.getElementById("alert");
 let buttonSomme = document.getElementById("somme");
+let div = document.createElement("div");
 let body = document.getElementById("boy");
-
 
 buttonAlert.addEventListener("click", () => alert("Hello world !"));
 
 buttonSomme.addEventListener("click", () => {
-    let div = document.createElement("div");
-    let body = document.getElementById("boy");
     div.innerHTML = 5+6;
     body.append(div);
 })
