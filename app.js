51c1a5f85010faac77da52ae0bfe1e5f6852496e
Clems-web <clement.servais1@gmail.com>
2020-12-20 17:34:13 +0100

    Finito !

---
+++ app.js
@@ -0,0 +1,15 @@
+let buttonAlert = document.getElementById("alert");
+let buttonSomme = document.getElementById("somme");
+let body = document.getElementById("boy");
+
+
+buttonAlert.addEventListener("click", () => {
+    alert("Hello world !");
+})
+
+buttonSomme.addEventListener("click", () => {
+    let div = document.createElement("div");
+    let body = document.getElementById("boy");
+    div.innerHTML = 5+6;
+    body.append(div);
+})
